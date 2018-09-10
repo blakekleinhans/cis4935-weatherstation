085f3623fa1b725df7a7aa3fabd2e2b2ce4121ee
Blake Kleinhans <Blake@Kleinhansgroup.com>
2018-09-10 15:03:30 -0400

    Started Implementing APIController

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -16,7 +16,103 @@ class APIController extends Controller
      */
     public function index(Request $request)
     {
-        $input = $request->input();
-        return var_dump($input);
+        // Sanitize and save raw data
+
+
+        // Handle legitimate requests
+        $inputs = $request->input();
+        // TODO: Compare against input keys
+        if(isset($inputs['action'])) {
+            // Handle request by action parameter
+            switch ($inputs['action']) {
+                case 'stationReadings':
+                    $result = $this->actionStationReadings($inputs);
+                    break;
+                case 'stationDetails':
+                    $result = $this->actionStationDetails($inputs);
+                    break;
+                case 'sensorDetails':
+                    $result = $this->actionSensorDetails($inputs);
+                    break;
+                default:
+                    $result = "Invalid Action";
+            }
+
+        } else {
+            $result = "Invalid Parameters!";
+        }
+
+        return $result;
+    }
+
+
+    // Handle 'stationReadings' action
+    protected function actionStationReadings($inputs) {
+        // Verify parameters
+        if($this->checkVars(['station_id'], $inputs)) {
+            // Clean Up Inputs
+            $station = $this->verifyStation($inputs['station_id']);
+            $readings = $this->verifyReadings($inputs);
+
+            // Store results of saveReadings for return
+            $result = $this->saveReadings($station, $readings);
+        } else {
+            $result = "Invalid stationReadings parameters";
+        }
+        return $result;
+    }
+
+    // Handle 'stationDetails' action
+    protected function actionStationDetails($inputs) {
+        // Store station details
+    }
+
+    // Handle 'sensorDetails' action
+    protected function actionSensorDetails($inputs) {
+        // Store sensor details
+    }
+
+    // Make sure that we have all of the required variables
+    // TODO: Improve
+    protected function checkVars($key, $inputs) {
+        foreach($key as $id) {
+            if(!isset($inputs[$id])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Store all sensor readings
+    protected function saveReadings($station, $readings) {
+        // Make batch
+        // TODO: Implement
+
+        // TODO: Properly assign
+        $batch_id = 1;
+
+        // Save sensor readings to database
+        foreach($readings as $id => $reading) {
+            DB::table('sensor_readings')->insert([
+                'batch_id'  => $batch_id,
+                'sensor_id' => $station['id'],
+                'value'     => $readings,
+            ]);
+        }
+    }
+
+    // Handle unknown stations
+    protected function verifyStation($id) {
+        // Check if station is saved in database
+
+        // Request station details
+
+        // Store new station
+
+    }
+
+    // Handle unknown sensors
+    protected function verifySensors($ids) {
+
     }
 }
