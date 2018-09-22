f0d9cd208b58d7e06f2a4aea8dbd84cf9f6ba39f
blake <blake@kleinhansgroup.com>
2018-09-22 18:24:39 -0400

    Seed Station 1 Sensors

--- database/seeds/SensorsTableSeeder.php
+++ database/seeds/SensorsTableSeeder.php
@@ -4,18 +4,27 @@ use Illuminate\Database\Seeder;
 
 class SensorsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    /** Run the database seeds.
+     * @return void */
     public function run()
     {
-        //
-	    DB::table('sensor_details')->insert([
-	    	'station_id' => 1,
-	    	'name' => 'Test Sensor',
-		    'is_active' => true,
-	    ]);
+        // Populate Station 1 Sensors
+        for($i=0; $i<count($this->station_1_sensors); $i++) {
+            DB::table('sensor_details')->insert([
+                'station_id' => 1,
+                'name' => $this->station_1_sensors[$i],
+                'is_active' => true,
+            ]);
+        }
     }
+    /** Default sensor names for station 1
+     * @var array */
+    protected $station_1_sensors = [
+        'Humidity',
+        'Pressure',
+        'Temperature',
+        'Rainfall',
+        'Wind Speed',
+        'Wind Direction',
+    ];
 }
