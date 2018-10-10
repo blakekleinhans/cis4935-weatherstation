05bb0520cf0eea5b1842100092d02fe048b5352c
blake <blake@kleinhansgroup.com>
2018-10-10 14:34:16 -0400

    Setup Laravel Mix for Chart.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>CIS4935: The Weatherman - @yield('title')</title>
-
     <link href = {{ asset("css/bootstrap.min.css") }} rel="stylesheet" />
     <link href = {{ asset("css/dashboard.css") }} rel="stylesheet" />
 </head>
 <body>
 @include('layouts.navbar')
-
 <div id="container-fluid">
     <div class="row">
         @include('layouts.sidebar', ['sidebarOptionsMain' => $sidebarOptionsMain, 'sidebarOptionsSensors' => $sidebarOptionsSensors])
@@ -21,8 +18,5 @@
         </main>
     </div>
 </div>
-
-<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
